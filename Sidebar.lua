733f10729a0e267c97094189afec820eb940fdc4
Tekkub <tekkub@gmail.com>
2008-08-10 18:56:51 -0600

    Get the sidebar working too

--- Sidebar.lua
+++ Sidebar.lua
@@ -43,7 +43,7 @@ end
 
 local anch1 = MultiBarRightButton1
 for actionID=36,25,-1 do
-	local mainbtn = factory("tekPopbar"..actionID, UIParent, "ActionBarButtonTemplate,SecureAnchorEnterTemplate")
+	local mainbtn = factory("tekPopbar"..actionID, UIParent, "ActionBarButtonTemplate,SecureHandlerEnterLeaveTemplate")
 --~ 	local mainbtn = CreateFrame("CheckButton", "tekPopbar"..actionID, UIParent, "ActionBarButtonTemplate,SecureAnchorEnterTemplate")
 --~ 	_G["tekPopbar"..actionID.."Name"]:Hide()
 --~ 	_G["tekPopbar"..actionID.."Name"].Show = _G["tekPopbar"..actionID.."Name"].Hide
@@ -57,23 +57,40 @@ for actionID=36,25,-1 do
 --~ 	mainbtn:HookScript("OnLeave", HideTooltip)
 	mainbtn:SetAttribute("*type*", "action")
 	mainbtn:SetAttribute("*action*", actionID)
-	mainbtn:SetAttribute("*childraise-OnEnter", true)
-	mainbtn:SetAttribute("*childstate-OnEnter", "enter")
-	mainbtn:SetAttribute("*childstate-OnLeave", "leave")
+--~ 	mainbtn:SetAttribute("*childraise-OnEnter", true)
+--~ 	mainbtn:SetAttribute("*childstate-OnEnter", "enter")
+--~ 	mainbtn:SetAttribute("*childstate-OnLeave", "leave")
 
-	local hdr = CreateFrame("Frame", "tekPopbarHdr"..actionID, mainbtn, "SecureStateHeaderTemplate")
-	hdr:SetPoint("CENTER") hdr:SetWidth(2) hdr:SetHeight(2)
-	hdr:SetAttribute("statemap-anchor-enter", "1")
-	hdr:SetAttribute("statemap-anchor-leave", ";")
-	hdr:SetAttribute("delaystatemap-anchor-leave", "1:0")
-	hdr:SetAttribute("delaytimemap-anchor-leave",  "1:1")
-	hdr:SetAttribute("delayhovermap-anchor-leave", "1:true")
-	mainbtn:SetAttribute("anchorchild", hdr)
+--~ 	local hdr = CreateFrame("Frame", "tekPopbarHdr"..actionID, mainbtn, "SecureStateHeaderTemplate")
+--~ 	hdr:SetPoint("CENTER") hdr:SetWidth(2) hdr:SetHeight(2)
+--~ 	hdr:SetAttribute("statemap-anchor-enter", "1")
+--~ 	hdr:SetAttribute("statemap-anchor-leave", ";")
+--~ 	hdr:SetAttribute("delaystatemap-anchor-leave", "1:0")
+--~ 	hdr:SetAttribute("delaytimemap-anchor-leave",  "1:1")
+--~ 	hdr:SetAttribute("delayhovermap-anchor-leave", "1:true")
+--~ 	mainbtn:SetAttribute("anchorchild", hdr)
+
+	mainbtn:SetAttribute("_execute", [[buttons = newtable()]])
+	mainbtn:SetAttribute("_onenter", [[for _,button in pairs(buttons) do button:Show() end]])
+	mainbtn:SetAttribute("_onleave", [[
+		elap = 0
+		return nil, nil, true
+	]])
+	mainbtn:SetAttribute("_onupdate", [[
+		if self:IsUnderMouse(true) then return nil, nil, true end
+
+		elap = elap + elapsed
+		if elap >= 2 then
+			for _,button in pairs(buttons) do button:Hide() end
+			return
+		end
+		return nil, nil, true
+	]])
 
 	local anch2 = mainbtn
 	for _,bar in ipairs(usebars) do
 		local btnID = actionID - 36 + bar*12
-		local btn = factory("tekPopbar"..btnID, hdr, "ActionBarButtonTemplate")
+		local btn = factory("tekPopbar"..btnID, mainbtn, "ActionBarButtonTemplate")
 --~ 		_G["tekPopbar"..btnID.."Name"]:Hide()
 --~ 		_G["tekPopbar"..btnID.."Name"].Show = _G["tekPopbar"..btnID.."Name"].Hide
 --~ 		ids[btn] = btnID
@@ -81,11 +98,18 @@ for actionID=36,25,-1 do
 --~ 		onupdates[btn] = btn:GetScript("OnUpdate")
 --~ 		btn:SetScript("OnUpdate", OnUpdate)
 --~ 		btn:SetScript("OnAttributeChanged", ActionButton_Update)
-		btn:SetAttribute("hidestates", 0)
+--~ 		btn:SetAttribute("hidestates", 0)
 		btn:SetAttribute("*type*", "action")
 		btn:SetAttribute("*action*", btnID)
-		hdr:SetAttribute("addchild", btn)
+--~ 		hdr:SetAttribute("addchild", btn)
 		btn:SetPoint("RIGHT", anch2, "LEFT", gap, 0)
+
+		btn:Hide()
+
+		mainbtn:SetAttribute("_adopt", btn)
+		mainbtn:SetAttribute("_frame-kid", btn)
+		mainbtn:SetAttribute("_execute", "buttons["..bar.."] = self:GetAttribute('frameref-kid')")
+
 		anch2 = btn
 	end
 
@@ -97,9 +121,9 @@ tekPopbar36:ClearAllPoints()
 tekPopbar36:SetPoint("BOTTOMRIGHT", WorldFrame, "BOTTOMRIGHT", 0, -gap)
 
 local function movetracker()
-		QuestWatchFrame:ClearAllPoints()
-		QuestWatchFrame:SetPoint("TOP", MinimapCluster, "BOTTOM", 0, 0)
-		QuestWatchFrame:SetPoint("RIGHT", tekPopbar25, "LEFT", -6, 0)
+	QuestWatchFrame:ClearAllPoints()
+	QuestWatchFrame:SetPoint("TOP", MinimapCluster, "BOTTOM", 0, 0)
+	QuestWatchFrame:SetPoint("RIGHT", tekPopbar25, "LEFT", -6, 0)
 end
 
 hooksecurefunc("UIParent_ManageFramePositions", movetracker)
